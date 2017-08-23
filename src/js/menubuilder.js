9a975e9d77619c88aa1fb0e15c8cebb1d1902d21
gugas02 <gugas02@hotmail.com>
2017-08-23 01:19:08 -0300

    submenu drag'n'drop creating

--- src/js/menubuilder.js
+++ src/js/menubuilder.js
@@ -41,3 +41,48 @@ function addToolbarItem(graph, prototype, image)
 	mxUtils.makeDraggable(img, graph, funct);
 	return img;
 }
+
+
+
+function buildmenuvariavel(graph){
+	var addVertex = function(icon)
+	{
+		var vertex = new mxCell(icon, new mxGeometry(0, 0, 40, 40), "");
+		vertex.setVertex(true);
+		vertex.setConnectable(false);
+		
+		var img = addToolbarItemVariavel(graph, vertex, icon);
+		
+	};
+	addVertex('char');
+	addVertex('uchar');
+	addVertex('int');
+	addVertex('uint');
+	addVertex('short');
+	addVertex('ushort');
+	addVertex('shortl');
+	addVertex('ushortl');
+	addVertex('long');
+	addVertex('ulong');
+	addVertex('flt');
+	addVertex('ptr');
+}
+function addToolbarItemVariavel(graph, prototype, image)
+{
+	var funct = function(graph, evt, cell, x, y)
+	{
+		var v11 = new mxCell(null, new mxGeometry(0, 15, 8, 10),style);
+		v11.setVertex(true);
+		v11.geometry.offset = new mxPoint(40, 0);
+		graph.stopEditing(false);
+		var vertex = graph.getModel().cloneCell(prototype);
+		vertex.geometry.x = x;
+		vertex.geometry.y = y;
+		graph.addCell(vertex);
+		graph.addCell(v11,vertex)
+		graph.setSelectionCell(vertex);
+	}
+	var img = document.getElementById(image)
+	mxUtils.makeDraggable(img, graph, funct);
+	return img;
+}
